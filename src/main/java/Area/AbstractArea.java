fb3cd2353068b567b2782297efe4dcc85782b6c4
Karina <kserranoa16@gmail.com>
2020-10-09 15:21:23 -0600

    Modificacion clase abstract

--- src/main/java/Area/AbstractArea.java
+++ src/main/java/Area/AbstractArea.java
@@ -3,13 +3,9 @@ package Area;
 
 
 public abstract class AbstractArea {
-    private double area;
     
-    private AbstractArea(double area){
-        this.area = area;
-    }
-    public void setArea(){
-        this.area = area;
+    public void calculoArea(){
+        
     }
     
 }
