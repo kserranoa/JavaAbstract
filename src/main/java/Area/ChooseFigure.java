b24243b131190b2d6af543d163941cb7eb9728ff
Karina <kserranoa16@gmail.com>
2020-10-09 18:59:16 -0600

    Creacion del objeto de clase SquareArea

--- src/main/java/Area/ChooseFigure.java
+++ src/main/java/Area/ChooseFigure.java
@@ -7,7 +7,8 @@ public class ChooseFigure {
 
     public static void main(String[] args) {
         
-        
+        SquareArea cuadrado = new SquareArea(10);
+        System.out.println(cuadrado.calculoArea());
     }
 
 }
