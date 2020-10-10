a7e5aa67bb60897a656c35a0328895c01eafd9b7
Karina <kserranoa16@gmail.com>
2020-10-09 19:35:04 -0600

    Metodo de calculo area circulo

--- src/main/java/Area/CircleArea.java
+++ src/main/java/Area/CircleArea.java
@@ -2,6 +2,17 @@
 package Area;
 
 
-public class CircleArea {
-
+public class CircleArea extends AbstractArea{
+    // Clase hereda abstracta para calculo area Circulo
+    
+    private double radio; 
+    
+    public CircleArea(double radio){
+        this.radio = radio;
+    }
+    
+    public double calculoArea(){
+        return Math.pow((Math.PI * radio),2);
+    }
+    
 }
