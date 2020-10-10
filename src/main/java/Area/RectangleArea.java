17ab3bac9aae91d985eec2eeb1795fe5f401257a
Karina <kserranoa16@gmail.com>
2020-10-09 19:30:59 -0600

    Metodo de calculo area rectangulo

--- src/main/java/Area/RectangleArea.java
+++ src/main/java/Area/RectangleArea.java
@@ -2,6 +2,16 @@
 package Area;
 
 
-public class RectangleArea {
-
+public class RectangleArea extends AbstractArea{
+     // Clase hereda abstracta para calculo area Rectangulo
+    private double longitud, ancho;
+    
+    public RectangleArea(double longitud, double ancho){
+        this.longitud = longitud; 
+        this.ancho = ancho;
+    }
+    
+    public double calculoArea(){
+        return longitud * ancho;
+    }
 }
