971409472f178037ad06e0cad1f81c975f549ec8
Karina <kserranoa16@gmail.com>
2020-10-09 19:21:40 -0600

    Creacion objeto para realizar calculo area triangulo

--- src/main/java/Area/ChooseFigure.java
+++ src/main/java/Area/ChooseFigure.java
@@ -9,6 +9,8 @@ public class ChooseFigure {
         
         SquareArea cuadrado = new SquareArea(10);
         System.out.println(cuadrado.calculoArea());
+        TriangleArea triangulo = new TriangleArea(5,2);
+        System.out.println(triangulo.calculoArea());
     }
 
 }
