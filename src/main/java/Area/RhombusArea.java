ab05e0ffeb96bec948c873abc74c9c39651efa39
Karina <kserranoa16@gmail.com>
2020-10-09 19:24:44 -0600

    Creacion del metodo calcula area Rombo

--- src/main/java/Area/RhombusArea.java
+++ src/main/java/Area/RhombusArea.java
@@ -2,6 +2,15 @@
 package Area;
 
 
-public class RhombusArea {
-
+public class RhombusArea extends AbstractArea{
+    private double lado, altura;
+    
+    public RhombusArea(double lado, double altura){
+        this.lado = lado; 
+        this.altura = altura; 
+    }
+    
+    public double calculoArea(){
+        return lado * altura;
+    }
 }
