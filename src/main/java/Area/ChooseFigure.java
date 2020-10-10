87bdddfaaf5bfbd0d761d4a26b15d604b74fe0ff
Karina <kserranoa16@gmail.com>
2020-10-09 19:27:11 -0600

    Comentarios agregados y objeto de rombo

--- src/main/java/Area/ChooseFigure.java
+++ src/main/java/Area/ChooseFigure.java
@@ -7,10 +7,18 @@ public class ChooseFigure {
 
     public static void main(String[] args) {
         
+        // Objeto calcula area cuadrado
         SquareArea cuadrado = new SquareArea(10);
         System.out.println(cuadrado.calculoArea());
+        
+        // Objeto calcula area triangulo
         TriangleArea triangulo = new TriangleArea(5,2);
         System.out.println(triangulo.calculoArea());
+        
+        // Objeto calcula area rombo
+        RhombusArea rombo = new RhombusArea(4,5);
+        System.out.println(rombo.calculoArea());
+        
     }
 
 }
