2e176430fff2da4e1a966618088fea85481fe121
Karina <kserranoa16@gmail.com>
2020-10-09 19:26:57 -0600

    Comentarios agregados

--- src/main/java/Area/RhombusArea.java
+++ src/main/java/Area/RhombusArea.java
@@ -3,6 +3,7 @@ package Area;
 
 
 public class RhombusArea extends AbstractArea{
+    // Clase hereda abstracta para calculo area Rombo
     private double lado, altura;
     
     public RhombusArea(double lado, double altura){
