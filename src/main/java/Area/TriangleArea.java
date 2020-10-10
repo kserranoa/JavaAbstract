2c609f3b97312d5baeba4d76c72bf682dee5241e
Karina <kserranoa16@gmail.com>
2020-10-09 19:20:53 -0600

    Creacion metodo que calcule el area del triangulo

--- src/main/java/Area/TriangleArea.java
+++ src/main/java/Area/TriangleArea.java
@@ -2,6 +2,15 @@
 package Area;
 
 
-public class TriangleArea {
-
+public class TriangleArea extends AbstractArea{
+    private double altura, lado;
+    
+    public TriangleArea(double a, double l){
+        altura = a;
+        lado = l; 
+    }
+    
+    public double calculoArea(){
+        return (lado*altura) * 0.5;
+    }
 }
