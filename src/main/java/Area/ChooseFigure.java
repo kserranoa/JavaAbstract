d40d9949f8ba1364f29fa3f934731ce528c74e66
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-10-11 13:32:00 -0600

    Ordenar codigo

--- src/main/java/Area/ChooseFigure.java
+++ src/main/java/Area/ChooseFigure.java
@@ -8,27 +8,25 @@ package Area;
 
 public class ChooseFigure {
     //Permite heredar clase abstrata
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {        
         // Objeto calcula area cuadrado
-        SquareArea cuadrado = new SquareArea(10);
-        System.out.println(cuadrado.calculoArea());
+        SquareArea cuadrado = new SquareArea(10);        
+        System.out.println("El area del cuadrado es:  " + cuadrado.calculoArea());
         
         // Objeto calcula area triangulo
         TriangleArea triangulo = new TriangleArea(5,2);
-        System.out.println(triangulo.calculoArea());
+        System.out.println("El area del triangulo es:  " + triangulo.calculoArea());
         
         // Objeto calcula area rombo
         RhombusArea rombo = new RhombusArea(4,5);
-        System.out.println(rombo.calculoArea());
+        System.out.println("El area del rombto es:  " + rombo.calculoArea());
         
         // Objeto calcula area rectangulo
         RectangleArea rectangulo = new RectangleArea(10,3);
-        System.out.println(rectangulo.calculoArea());
+        System.out.println("El area del rectanguo es:  " + rectangulo.calculoArea());
         
         // Objeto calcula area circulo
         CircleArea circulo = new CircleArea(7);
-        System.out.println(circulo.calculoArea());
+        System.out.println("El area del circulo es:  " + circulo.calculoArea());
     }
-
 }
