ee585764c28e559f8bce245b689705d6a1907d72
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-10-11 13:18:37 -0600

    Ordenar codigo

--- src/main/java/Area/AbstractArea.java
+++ src/main/java/Area/AbstractArea.java
@@ -1,9 +1,6 @@
-
 package Area;
 
-
 public abstract class AbstractArea {
-    //Permite heredar metodo para calcular area de diferentes figuras
-    
+    //Permite heredar metodo para calcular area de diferentes figuras    
     public abstract double calculoArea();
 }
